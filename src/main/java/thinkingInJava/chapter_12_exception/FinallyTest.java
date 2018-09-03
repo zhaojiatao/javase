93726087de3392fe0f51da1a714ca30764fe3b6a
zhaojiatao <2322795718@qq.com>
2018-09-03 17:03:39 +0800

    Initial commit

---
+++ src/main/java/thinkingInJava/chapter_12_exception/FinallyTest.java
@@ -0,0 +1,73 @@
+package thinkingInJava.chapter_12_exception;
+
+/**
+ * @author zhaojiatao
+ * @date 2018/9/2
+ *
+ *
+ * 这个类主要说明，当finally中有return和没有retrun，返回基本类型和饮用类型的区别；
+ *
+ * 当finally中有return时，最终一定从finally中返回；
+ * 当finally中没有return时：
+ *      当返回引用类型时：finally中对返回值的修改会覆盖返回值；
+ *      当返回基本类型时：finally中对返回值的修改不会覆盖返回值；
+ *
+ */
+public class FinallyTest {
+
+    public static void main(String[] args) {
+        System.out.println(test());
+        System.out.println(testPrimitive());
+    }
+
+    public static Student test() {
+        Student result = new Student("Tom", 0);
+        try {
+            result.setAge(1);
+            //System.out.println(1/0);
+            return result;
+        } catch (Exception e) {
+            result.setAge(2);
+            return result;
+        } finally {
+            result.setAge(3);       //引用类型的返回值，可被修改
+            result=new Student("Kobe", 33);
+            //return new Student("Kobe", 33);   //可以被“具体值”覆盖
+        }
+
+    }
+
+    public static int testPrimitive() {
+        int x = 0;
+        try {
+            x = 1;
+            //System.out.println(1/0);
+            return x;
+        } catch (Exception e) {
+            x = 2;
+            return x;
+        } finally {
+            x = 3;          //基本类型的返回值，不可被修改
+            //return x;    //可以被“具体值”覆盖
+        }
+
+    }
+
+    private static class Student {
+        private String name;
+        private int age;
+
+        public Student(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+        @Override
+        public String toString() {
+            return "Student [name=" + name + ", age=" + age + "]";
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+    }
+}
---
+++ src/main/java/thinkingInJava/chapter_3_object/equalsLearn.java
@@ -0,0 +1,61 @@
+package thinkingInJava.chapter_3_object;
+
+/**
+ * @author zhaojiatao
+ * @date 2018/7/19
+ */
+public class equalsLearn {
+
+    public static void main(String[] args) {
+
+        /**
+         * 当基本类型的比较时：只能用==，比较值；
+         * 当是引用类型比较时：如果未重写equals方法，那么equals用的是来自object类的equals方法，就是==，即比较内存地址
+         * 当是String类型时，如果是由String str="xxx"；这样直接赋值的话，比较的是字符串的值；
+         *                 如果由String str=new String("xxx")；方式new出来的对象的话，比较时是用的String类重写的equals方法；
+         * 注意，toString方法返回的new String()对象；
+         *
+         */
+
+        int x=1;
+        int y=1;
+
+        // 基本类型判断值是否相等
+        System.out.println(x==y);// true
+
+        //java会自动拆箱，由编译器完成，实际上判断的是x==1
+        System.out.println(x==new Integer(1));// true
+
+        //比较两个对象的内存地址
+        System.out.println(new Integer(1)==new Integer(1));// false
+
+        //使用Integer包装类重写的equals方法比较
+        System.out.println(new Integer(1).equals(new Integer(1)));//true
+
+
+
+        String str1="abc";
+        String str2="abc";
+
+        // 比较两个字符串的值
+        System.out.println(str1==str2);// true
+
+        // 比较两个字符串 使用String类重写的equeals方法
+        System.out.println(str1.equals(str2));// true
+
+        StringBuffer str3=new StringBuffer("efg");
+        StringBuffer str4=new StringBuffer("efg");
+
+        // 比较内存地址
+        System.out.println(str3==str4);//false
+        // 比较内存地址
+        System.out.println(str3.equals(str4));// false 由于StringBuffer类没有重写Object类的equals，故比较的是内存地址
+
+        // toString会生成new String()对象
+        System.out.println(str3.toString()==str4.toString());//false 比较内存地址
+        System.out.println(str3.toString().equals(str4.toString()));//true 使用String对象重写的equals比较
+
+    }
+
+
+}
