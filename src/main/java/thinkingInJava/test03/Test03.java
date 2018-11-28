66887b46a1f7e08984fd0e7bf89c5c53fa01fd98
zhaojiatao <2322795718@qq.com>
2018-11-28 10:24:10 +0800

    新建02分支测试

---
+++ src/main/java/thinkingInJava/test03/Test03.java
@@ -0,0 +1,11 @@
+package thinkingInJava.test03;
+
+/**
+ * @author zhaojiatao
+ * @date 2018/11/28
+ */
+public class Test03 {
+    public static void main(String[] args) {
+        System.out.println(11);
+    }
+}
