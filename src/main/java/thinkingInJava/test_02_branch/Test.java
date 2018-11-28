455abe52005dbbd51135bd5620fbf8cbe34f973f
zhaojiatao <2322795718@qq.com>
2018-11-28 10:22:51 +0800

    新建02分支测试

---
+++ src/main/java/thinkingInJava/test_02_branch/Test.java
@@ -0,0 +1,18 @@
+package thinkingInJava.test_02_branch;
+
+/**
+ * @author zhaojiatao
+ * @date 2018/11/28
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(11111);
+    }
+
+
+
+
+
+
+}
