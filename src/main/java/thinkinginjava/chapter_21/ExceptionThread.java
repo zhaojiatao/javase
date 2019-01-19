685511c4b6321ec3da36586f4ad92e1902fec78c
zhaojiatao <2322795718@qq.com>
2019-01-19 14:47:20 +0800

    捕获线程逃逸的异常

---
+++ src/main/java/thinkinginjava/chapter_21/ExceptionThread.java
@@ -0,0 +1,24 @@
+package thinkinginjava.chapter_21;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author zhaojiatao
+ * @date 2019/1/19
+ */
+public class ExceptionThread implements Runnable{
+    @Override
+    public void run() {
+        throw new RuntimeException();
+    }
+
+    public static void main(String[] args) {
+        try{
+            ExecutorService exec= Executors.newCachedThreadPool();
+            exec.execute(new ExceptionThread());
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+}
