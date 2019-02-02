941a72e9e34f1ed4a26251df758d17e72d7d50c4
zhaojiatao <2322795718@qq.com>
2019-02-02 21:56:57 +0800

    终结任务，装饰性花园

---
+++ src/main/java/thinkinginjava/chapter_21/Count.java
@@ -0,0 +1,95 @@
+package thinkinginjava.chapter_21;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zhaojiatao
+ * @date 2019/2/2
+ */
+public class Count {
+
+    private int count=0;
+    private Random rand=new Random(47);
+    public synchronized int increment(){
+        int temp=count;
+        if(rand.nextBoolean()){
+            Thread.yield();
+        }
+        return (count=++temp);
+    }
+    public synchronized int value(){
+        return count;
+    }
+}
+
+class Entrance implements Runnable{
+    private static Count count=new Count();
+    private static List<Entrance> entrances=new ArrayList<>();
+    private int number=0;
+    private final int id;
+    private static volatile boolean canceled=false;
+    public static void cancel(){
+        canceled=true;
+    }
+    public Entrance(int id){
+        this.id=id;
+        entrances.add(this);
+    }
+
+    @Override
+    public void run() {
+        while (!canceled){
+            synchronized (this){
+                ++number;
+            }
+            System.out.println(this+" Total: "+count.increment());
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Stopping "+this);
+    }
+    public synchronized int getValue(){
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Entrance "+id+": "+getValue();
+    }
+    public static int getTotalCount(){
+        return count.value();
+    }
+    public static int sumEntrances(){
+        int sum=0;
+        for (Entrance entrance:entrances){
+            sum += entrance.getValue();
+        }
+        return sum;
+    }
+}
+
+class OrnametalGarden{
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService exec= Executors.newCachedThreadPool();
+        for (int i=0;i<5;i++){
+            exec.execute(new Entrance(i));
+        }
+        TimeUnit.SECONDS.sleep(3);
+        Entrance.cancel();
+        exec.shutdown();
+        if(!exec.awaitTermination(250,TimeUnit.MILLISECONDS)){
+            System.out.println("Some tasks were not terminated");
+        }
+        System.out.println("Total: "+Entrance.getTotalCount());
+        System.out.println("Sum of Entrances: "+Entrance.sumEntrances());
+    }
+}
+
