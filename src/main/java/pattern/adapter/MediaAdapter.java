0c9087f1c1e9b112c6b29ad0de629e218e625d21
zhaojiatao <2322795718@qq.com>
2019-02-27 10:20:58 +0800

    适配器模式

---
+++ src/main/java/pattern/adapter/MediaAdapter.java
@@ -0,0 +1,28 @@
+package pattern.adapter;
+
+/**
+ * @author zhaojiatao
+ * @date 2019/2/27
+ * 创建实现了 MediaPlayer 接口的适配器类
+ */
+public class MediaAdapter implements MediaPlayer {
+
+    AdvancedMediaPlayer advancedMusicPlayer;
+
+    public MediaAdapter(String audioType){
+        if(audioType.equalsIgnoreCase("vlc") ){
+            advancedMusicPlayer = new VlcPlayer();
+        } else if (audioType.equalsIgnoreCase("mp4")){
+            advancedMusicPlayer = new Mp4Player();
+        }
+    }
+
+    @Override
+    public void play(String audioType, String fileName) {
+        if(audioType.equalsIgnoreCase("vlc")){
+            advancedMusicPlayer.playVlc(fileName);
+        }else if(audioType.equalsIgnoreCase("mp4")){
+            advancedMusicPlayer.playMp4(fileName);
+        }
+    }
+}
