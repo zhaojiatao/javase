ef1cece3ff29c54ada1682fc2448f8085c65a4e2
zhaojiatao <2322795718@qq.com>
2019-09-22 00:56:10 +0800

    动态代理使用案例

--- src/main/java/java7/class2/part3/GreetFactory.java
+++ src/main/java/java7/class2/part3/GreetFactory.java
@@ -1,8 +1,11 @@
 package java7.class2.part3;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
+ * 本篇幅代码主要讲述动态代理的实用案例：
  * @author zhaojiatao
  * @date 2019-09-21
  * 在开发新版本的时候发现旧版本的GreetV1不合理，才设计了V2版本
@@ -10,29 +13,82 @@ import java.lang.reflect.Proxy;
  * 通过动态代理，可以把实现了旧接口GreetV1的对象实例转换成可以通过新接口GreetV2来调用。
  * 这样，既保证了对新接口的使用，又使旧接口的实现可以继续存在。
  * 实际上是通过动态代理实现适配器模式。
+ * 实现这样的动态代理的关键就在于适配两个接口的InvocationHandler，见GreetAdapter
  */
 public class GreetFactory {
 
     /**
      * 每一个GreetV1接口的实现，都可以通过此工厂方法转换成通过GreeV2接口来使用的新对象
+     *
      * @param greet
      * @return
      */
-    public static GreetV2 adaptGreet(GreetV1 greet){
-        GreetAdapter adapter=new GreetAdapter(greet);
-        ClassLoader cl=greet.getClass().getClassLoader();
-        return (GreetV2) Proxy.newProxyInstance(cl,new Class<?>[]{GreetV2.class},adapter);
+    public static GreetV2 adaptGreet(GreetV1 greet) {
+        GreetAdapter adapter = new GreetAdapter(greet);
+        ClassLoader cl = greet.getClass().getClassLoader();
+        return (GreetV2) Proxy.newProxyInstance(cl, new Class<?>[]{GreetV2.class}, adapter);
     }
 
     public static void main(String[] args) {
 
-        GreetV2 greetV2= adaptGreet(new GreetV1() {
-            @Override
-            public String greet(String name, String gender) throws RuntimeException {
-                return name+gender;
-            }
-        });
-        System.out.println( greetV2.greet("大山"));
+        GreetV1 greetV1 = new GreetV1Impl();
+
+        GreetV2 greetV2 = adaptGreet(greetV1);
+
+        System.out.println(greetV2.greet("大山"));
 
     }
+}
+
+interface GreetV1 {
+    String greet(String name, String gender) throws RuntimeException;
+
+}
+
+class GreetV1Impl implements GreetV1{
+
+    @Override
+    public String greet(String name, String gender) throws RuntimeException {
+        return name + gender;
+    }
+}
+
+interface GreetV2 {
+    String greet(String username);
+
+}
+
+class GreetAdapter implements InvocationHandler {
+
+    private GreetV1 greetV1;
+
+    public GreetAdapter(GreetV1 greetV1) {
+        this.greetV1 = greetV1;
+    }
+
+    /**
+     * 要理解这个invoke方法其实不是代理对象的执行，而是生成的代理实例的接口方法执行时触发的逻辑
+     * @param proxy
+     * @param method 代理实例执行了代理接口的哪个方法
+     * @param args
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        String methodName = method.getName();
+
+        if ("greet".equals(methodName)) {
+            String username = (String) args[0];
+            //name和gender这里演示的意思是通过username可以计算得出
+            String name = username + "zhaojiatao";
+            String gender = username + "男";
+            Method greetMethodV1 = GreetV1.class.getMethod(methodName, new Class<?>[]{String.class, String.class});
+            return greetMethodV1.invoke(greetV1, new Object[]{name, gender});
+        } else {
+            return method.invoke(greetV1, args);
+        }
+    }
+
 }
--- src/main/java/java7/class2/part3/Test.java
+++ src/main/java/java7/class2/part3/Test.java
@@ -3,8 +3,10 @@ package java7.class2.part3;
 import com.alibaba.fastjson.JSON;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -14,86 +16,111 @@ import java.util.Set;
  */
 public class Test {
 
-    public static void useProxy(){
-        String str="Hello world";
+    public static void useProxy() {
+        String str = "Hello world";
         //创建动态代理时需要一个InvocationHandler接口的实现对象
-        LoggingInvocationHandler handler=new LoggingInvocationHandler(str);
+        LoggingInvocationHandler handler = new LoggingInvocationHandler(str);
 
-        Comparable proxy=makeProxy(Comparable.class,str,handler);
+        Comparable proxy = makeProxy(Comparable.class, str, handler);
         int result = proxy.compareTo("Good");
         System.out.println(result);
 
-        List<String> list=new ArrayList<String>();
-        handler=new LoggingInvocationHandler(list);
-        list=makeProxy(List.class,list,handler);
+        List<String> list = new ArrayList<String>();
+        handler = new LoggingInvocationHandler(list);
+        list = makeProxy(List.class, list, handler);
         list.add("Hello");
         System.out.println(JSON.toJSONString(list));
 
     }
 
     /**
-     * 通用的工厂方法：为任何object对象创建intf接口的代理
+     * 这是只有一个代理接口的生成代理的通用工厂方法
+     *
+     * 为任何object对象创建intf接口的代理
      * 大多数情况下，代理对象只会实现一个java接口
-     * @param intf 要代理的接口
-     * @param object 代理对象
+     *
+     * 一句话概括就是
+     * 生成 object对象 的intf接口 通过handler实现类处理方法逻辑的 代理实例对象
+     *
+     * @param intf    要代理的接口
+     * @param object  代理对象
      * @param handler 处理接口方法调用的InvocationHandler接口实现类对象
      * @param <T>
-     * @return
-     * 通过Proxy.newProxyInstance创建出来的代理对象只能转换成它所实现的接口类型，而不能转换成接口的具体实现类，因为动态代理只对接口起作用
+     * @return 通过Proxy.newProxyInstance创建出来的代理对象只能转换成它所实现的接口类型，而不能转换成接口的具体实现类，因为jdk动态代理只对接口起作用
+     * 一句话概括就是
+     * 生成 object对象 的intf接口 通过handler实现类处理方法逻辑的 代理实例对象
      */
-    public static <T> T makeProxy(Class<T> intf,final T object,InvocationHandler handler){
-        ClassLoader cl=object.getClass().getClassLoader();
-        return (T)Proxy.newProxyInstance(cl,new Class<?>[]{intf},handler);
+    public static <T> T makeProxy(Class<T> intf, final T object, InvocationHandler handler) {
+        //代理对象的类加载器，这个类加载器主要是生成代理时用来反射用的
+        ClassLoader cl = object.getClass().getClassLoader();
+        return (T) Proxy.newProxyInstance(cl, new Class<?>[]{intf}, handler);
     }
 
     /**
-     *
+     * 没有指定为代理对象指定具体哪个接口，而是为这个代理对象的所有接口生成通用的代理实例，用户可以自己根据需要转成自己想要的代理对象
      * @param object
      * @param handler
      * @return 并没有对创建的代理对象进行类型转换，而是直接返回给调用者。这是为了让调用者可以灵活操作，允许它们根据需要转换成不同的接口。
      * 比如，如果传入的是String类的对象实例，则调用者可以将其转换成String类所实现的Comparable或是CharSequence接口。
      */
-    public static Object proxyAll(final Object object,InvocationHandler handler){
-        ClassLoader cl=object.getClass().getClassLoader();
-        Class<?>[] interfaces= object.getClass().getInterfaces();
-        return Proxy.newProxyInstance(cl,interfaces,handler);
+    public static Object proxyAll(final Object object, InvocationHandler handler) {
+        //代理对象的类加载器，这个类加载器主要是生成代理时用来反射用的
+        ClassLoader cl = object.getClass().getClassLoader();
+        Class<?>[] interfaces = object.getClass().getInterfaces();
+        return Proxy.newProxyInstance(cl, interfaces, handler);
     }
 
     /**
      * 在同时代理多个接口时，这些接口在代理类创建时的排序非常重要，即使是同样的接口，不同的排序所产生的代理类也是不同的。
      * 强调接口的排序顺序的一个重要原因是，这个顺序会对接口中声明类型相同的方法的选择产生影响。
      * 如果多个接口中都存在声明类型相同的方法，那么在调用方法时，排列顺序中最先出现的接口中的方法会被选择。
+     *
      * @throws Throwable
      */
-    public static void proxyMultipleInterfaces() throws Throwable{
-        List<String> receiverObj=new ArrayList<String>();
-        ClassLoader cl=new Test().getClass().getClassLoader();
-        LoggingInvocationHandler handler=new LoggingInvocationHandler(receiverObj);
+    public static void proxyMultipleInterfaces() throws Throwable {
+        List<String> receiverObj = new ArrayList<String>();
+        ClassLoader cl = new Test().getClass().getClassLoader();
+        LoggingInvocationHandler handler = new LoggingInvocationHandler(receiverObj);
         //首先获取到代理类，得到代理类实现了被代理的接口
-        Class<?> proxyClass=Proxy.getProxyClass(cl,new Class<?>[]{List.class, Set.class});
+        Class<?> proxyClass = Proxy.getProxyClass(cl, new Class<?>[]{List.class, Set.class});
 //        Class<?> proxyClass=Proxy.getProxyClass(cl,new Class<?>[]{ Set.class,List.class});
-        Object proxy=proxyClass.getConstructor(new Class[]{InvocationHandler.class}).newInstance(new Object[]{handler});
+        Object proxy = proxyClass.getConstructor(new Class[]{InvocationHandler.class}).newInstance(new Object[]{handler});
 
-
-
-        List list=(List) proxy;
+        List list = (List) proxy;
         list.add("hello");
 
-        Set set=(Set)proxy;
+        Set set = (Set) proxy;
         set.add("World");
 
     }
-
-
-
-
-
 
     public static void main(String[] args) throws Throwable {
 //        useProxy();
         proxyMultipleInterfaces();
     }
 
+}
 
+class LoggingInvocationHandler implements InvocationHandler {
+
+    private Object receiverObject;
+
+    public LoggingInvocationHandler(Object receiverObject) {
+        this.receiverObject = receiverObject;
+    }
+
+    /**
+     * @param proxy  代理对象
+     * @param method 所调用方法的Method对象
+     * @param args   实际参数列表
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("调用方法" + method.getName() + ":参数为" + Arrays.deepToString(args));
+
+        return method.invoke(receiverObject, args);
+    }
 
 }
