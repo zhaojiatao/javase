5c0a3b055844f2f5bd734691a06ee65804166be2
zhaojiatao <2322795718@qq.com>
2019-10-07 22:54:39 +0800

    使用EnumSet替代标志

--- src/main/java/thinkinginjava/chapter_19_enum/part_19_8/AlarmPoints.java
+++ src/main/java/thinkinginjava/chapter_19_enum/part_19_8/AlarmPoints.java
@@ -5,6 +5,13 @@ import java.util.EnumSet;
 /**
  * @author zhaojiatao
  * @date 2019-10-07
+ *
+ * EnumSet和hashset相比更快
+ *
+ */
+
+/**
+ * 表示一座大楼里警报传感器的安放位置
  */
 public enum AlarmPoints {
 
@@ -18,16 +25,19 @@ class EnumSets{
 
         EnumSet<AlarmPoints> points=EnumSet.noneOf(AlarmPoints.class);
         points.add(AlarmPoints.BATHROOM);
-        System.out.println(points);
+        //[BATHROOM]
+        System.out.println("1:"+points);
         points.addAll(EnumSet.of(AlarmPoints.STAIR1,AlarmPoints.STAIR2,AlarmPoints.KITCHEN));
-        System.out.println(points);
+        //[STAIR1, STAIR2, BATHROOM, KITCHEN]
+        System.out.println("2:"+points);
         points=EnumSet.allOf(AlarmPoints.class);
+        System.out.println("3:"+points);
         points.removeAll(EnumSet.of(AlarmPoints.STAIR1,AlarmPoints.STAIR2,AlarmPoints.KITCHEN));
-        System.out.println(points);
+        System.out.println("4:"+points);
         points.removeAll(EnumSet.range(AlarmPoints.OFFICE1,AlarmPoints.OFFICE4));
-        System.out.println(points);
+        System.out.println("5:"+points);
         points=EnumSet.complementOf(points);
-        System.out.println(points);
+        System.out.println("6:"+points);
 
 
     }
