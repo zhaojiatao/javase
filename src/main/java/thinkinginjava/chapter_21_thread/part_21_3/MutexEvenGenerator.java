1f475861c04618342d50dc860bba47312aef2ee9
zhaojiatao <2322795718@qq.com>
2019-10-12 23:42:04 +0800

    用显示的Lock重写SynchronizedEvenGenerator

--- src/main/java/thinkinginjava/chapter_21_thread/part_21_3/MutexEvenGenerator.java
+++ src/main/java/thinkinginjava/chapter_21_thread/part_21_3/MutexEvenGenerator.java
@@ -6,12 +6,20 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * @author zhaojiatao
  * @date 2019/1/20
+ * 用显示的Lock重写SynchronizedEvenGenerator
  */
 public class MutexEvenGenerator extends IntGenerator {
 
     private int currentEvenValue=0;
+    /**
+     * 被互斥调用的锁
+     */
     private Lock lock=new ReentrantLock();
 
+    /**
+     * 注意：return语句必须在try子句中出现，以确保unlock不会过早发生，以防止将数据暴露给了第二个任务
+     * @return
+     */
     @Override
     public int next() {
         lock.lock();
