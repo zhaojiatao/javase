4a759643705e208652b9a46e2ff45264c503830d
zhaojiatao <2322795718@qq.com>
2019-10-14 22:21:56 +0800

    装饰性花园问题

--- src/main/java/thinkinginjava/chapter_21_thread/part_21_4/Count.java
+++ src/main/java/thinkinginjava/chapter_21_thread/part_21_4/Count.java
@@ -13,11 +13,19 @@ import java.util.concurrent.TimeUnit;
  */
 public class Count {
 
-    //count类的count字段存储的就是总计数
+    /**
+     * count类的count字段存储的就是总计数，代表公园的总人数
+     * 每次被一个线程调用一次increment方法，总人数会加一
+     * 不同的线程都在count对象上同步，所以increment方法线程安全
+     */
     private int count=0;
     private Random rand=new Random(47);
-    //每调用一次increment就会总计数增加一次，线程安全
-    //count字段时私有的，使用公共的increment和value方法来控制外部对count字段的访问
+
+    /**
+     * 每调用一次increment就会总计数增加一次，线程安全
+     * count字段是私有的，使用公共的increment和value方法来控制外部对count字段的访问
+     * @return
+     */
     public synchronized int increment(){
         int temp=count;
         if(rand.nextBoolean()){
@@ -25,25 +33,50 @@ public class Count {
         }
         return (count=++temp);
     }
-    //返回总计数
+
+    /**
+     * 返回总计数
+     * @return
+     */
     public synchronized int value(){
         return count;
     }
 }
 
-//线程类
+/**
+ * 公园入口类
+ */
 class Entrance implements Runnable{
     private static Count count=new Count();
+    /**
+     * 静态变量存储所有的入口对象
+     */
     private static List<Entrance> entrances=new ArrayList<>();
-    //每个线程对象有个对象的变量number，用来存储每个特定入口的参观量。最终各个线程的number变量的和应该等于count类的count字段的值。用来实现对count的双重校验
+    /**
+     * 每个线程对象有个对象的变量number，用来存储每个特定入口的参观量。
+     * 最终各个线程的number变量的和应该等于Count类的count字段的值。用来实现对count的双重校验
+     */
     private int number=0;
-    //每个线程对象的id
+    /**
+     * 每个线程对象的id
+     */
     private final int id;
-    //全局可见的类静态变量
+    /**
+     * 全局可见的类静态变量，表示公园是否闭馆
+     */
     private static volatile boolean canceled=false;
+
+    /**
+     * 闭馆方法
+     */
     public static void cancel(){
         canceled=true;
     }
+
+    /**
+     * 每新建一个线程的时候，会在线程集合中添加本线程，为了在打印时区分当前线程是哪个线程s
+     * @param id
+     */
     public Entrance(int id){
         this.id=id;
         //每新建一个线程的时候，会在线程集合中添加本线程
@@ -61,8 +94,8 @@ class Entrance implements Runnable{
             System.out.println(this+" Total: "+count.increment());
 
             //最终，每个线程的number字段的和应该等于求和工具类count的count字段的值。
-
             try {
+                //每个入口每个100毫秒进来一个人
                 TimeUnit.MILLISECONDS.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
