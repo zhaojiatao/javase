749c4ff0e0c30491d780d2b2796840296790cc6a
zhaojiatao <2322795718@qq.com>
2019-10-19 00:54:53 +0800

    延时队列原理

--- src/main/java/thinkinginjava/chapter_21_thread/part_21_7/DelayQueueDemo.java
+++ src/main/java/thinkinginjava/chapter_21_thread/part_21_7/DelayQueueDemo.java
@@ -7,6 +7,25 @@ import java.util.*;
 
 import static java.util.concurrent.TimeUnit.*;
 
+/**
+ * DelayQueue
+ * java延迟队列提供了在指定时间才能获取队列元素的功能，队列头元素是最接近过期的元素，即延时低的任务在队头。
+ * 没有过期元素的话，使用poll()方法会返回null值，超时判定是通过getDelay(TimeUnit.NANOSECONDS)方法的返回值小于等于0来判断。
+ * 延时队列不能存放空元素。
+ * 延时队列实现了Iterator接口，但iterator()遍历顺序不保证是元素的实际存放顺序。
+ *
+ * 由Delayed定义可以得知，队列元素需要实现getDelay(TimeUnit unit)方法和compareTo(Delayed o)方法,
+ * getDelay定义了剩余到期时间，compareTo方法定义了元素排序规则.
+ * 注意，元素的排序规则影响了元素的获取顺序，将在后面说明。
+ *
+ * 内部存储结构　
+ * DelayedQuene的元素存储交由优先级队列存放,DelayedQuene的优先级队列使用的排序方式是队列元素的compareTo方法，
+ * 优先级队列存放顺序是从小到大的，所以队列元素的compareTo方法影响了队列的出队顺序。
+ * 若compareTo方法定义不当，会造成延时高的元素在队头，延时低的元素无法出队。
+ *
+ * https://www.cnblogs.com/hhan/p/10678466.html
+ *
+ */
 class DelayedTask implements Runnable, Delayed {
     private static int counter = 0;
     private final int id = counter++;
@@ -16,9 +35,12 @@ class DelayedTask implements Runnable, Delayed {
     /**
      * sequence确保了任务被创建的顺序
      */
-    protected static List<DelayedTask> sequence =
-            new ArrayList<>();
+    protected static List<DelayedTask> sequence = new ArrayList<>();
 
+    /**
+     * 通过构造方法确定了这个延时任务要延时多久
+     * @param delayInMilliseconds
+     */
     public DelayedTask(int delayInMilliseconds) {
         delta = delayInMilliseconds;
         //任务初始化时的系统时间
@@ -97,6 +119,7 @@ class DelayedTaskConsumer implements Runnable {
     public void run() {
         try {
             while (!Thread.interrupted()) {
+                //移除并返回队列头部的元素
                 q.take().run(); // Run task with the current thread
             }
         } catch (InterruptedException e) {
@@ -110,14 +133,17 @@ public class DelayQueueDemo {
     public static void main(String[] args) {
         Random rand = new Random(47);
         ExecutorService exec = Executors.newCachedThreadPool();
-        DelayQueue<DelayedTask> queue = new DelayQueue<DelayedTask>();
+        //定义一个延迟队列，里面放置了20个延时任务，每个任务的延时时间是随机的
+        DelayQueue<DelayedTask> queue = new DelayQueue<>();
         // Fill with tasks that have random delays:
         for (int i = 0; i < 20; i++) {
             queue.put(new DelayedTask(rand.nextInt(5000)));
         }
 
         // Set the stopping point
+        //最后在队列中
         queue.add(new DelayedTask.EndSentinel(5000, exec));
+        //从任务队列中取任务并运行任务
         exec.execute(new DelayedTaskConsumer(queue));
     }
 } /* Output:
